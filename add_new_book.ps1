728901b2920a3ae3e4a830faff90df4cb8a636b0
drewmarsh <drewmmisc@gmail.com>
2024-02-17 17:59:07 -0500

    Add funtionality to auto-create config.json

--- add_new_book.ps1
+++ add_new_book.ps1
@@ -8,9 +8,9 @@ $ScriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
 $ConfigFileName = "config.json"
 $ConfigPath = Join-Path -Path $ScriptDirectory -ChildPath $ConfigFileName
 
-# Set default directory values
-$AudiobookDirectory = "C:\Audiobooks"
-$EbookDirectory = "C:\Ebooks"
+# Directory values
+$AudiobookDirectory
+$EbookDirectory
 
 # Load configuration if available
 if (Test-Path -Path $ConfigPath) {
@@ -29,25 +29,20 @@ function Save-Configuration {
     $Config | Set-Content -Path $ConfigPath
 }
 
-function Validate-Input {
+function Confirm-UserChoice {
     param (
         [string]$Prompt
     )
     do {
-        $input = Read-Host $Prompt
-    } while ($input -notmatch '^[YNyn]$')
-    return $input -imatch '[Yy]'
+        $userChoice = Read-Host $Prompt
+    } while ($userChoice -notmatch '^[YNyn]$')
+    return $userChoice -imatch '[Yy]'
 }
 
 do {
     # Prompt the user for input with input validation
-    $CreateAudiobook = Validate-Input "Create an audiobook directory? (Y/N)"
-    $CreateEbook = Validate-Input "Create an ebook directory? (Y/N)"
-
-    if (-not ($CreateAudiobook -or $CreateEbook)) {
-        Write-Host "No directory selected. Script exiting."
-        exit
-    }
+    $CreateAudiobook = Confirm-UserChoice "Create an audiobook directory? (Y/N)"
+    $CreateEbook = Confirm-UserChoice "Create an ebook directory? (Y/N)"
 
     # Prompt the user for Author, Book Title, and Release Year
     $Author = Read-Host "Enter Author"
@@ -56,29 +51,31 @@ do {
 
     # Set directory names based on user choices
     if ($CreateAudiobook) {
-        # Ask user to enter a Narrator only if an audiobook directory is being created
-        $Narrator = Read-Host "Enter Narrator"
-    }
-
-    # Verify audiobook directory
-    if ($CreateAudiobook) {
-        $AudiobookDirectoryCorrect = Validate-Input "Is this the correct location to store the audiobook in? $AudiobookDirectory (Y/N)"
-        if (-not $AudiobookDirectoryCorrect) {
+        if (-not (Test-Path -Path $ConfigPath)) {
             $AudiobookDirectory = Read-Host "Enter the desired directory for audiobooks"
+        } else {
+            $AudiobookDirectoryCorrect = Confirm-UserChoice "Is this the correct location to store the audiobook in? $AudiobookDirectory (Y/N)"
+            if (-not $AudiobookDirectoryCorrect) {
+                $AudiobookDirectory = Read-Host "Enter the desired directory for audiobooks"
+            }
         }
-        $FolderPathAudiobook = Join-Path -Path $AudiobookDirectory -ChildPath "$Author\$BookTitle ($ReleaseYear)\$Narrator"
-        Save-Configuration
+        $FolderPathAudiobook = Join-Path -Path $AudiobookDirectory -ChildPath "$Author\$BookTitle ($ReleaseYear)"
     }
 
-    # Verify ebook directory
     if ($CreateEbook) {
-        $EbookDirectoryCorrect = Validate-Input "Is this the correct base location to store the ebook in? $EbookDirectory (Y/N)"
-        if (-not $EbookDirectoryCorrect) {
+        if (-not (Test-Path -Path $ConfigPath)) {
             $EbookDirectory = Read-Host "Enter the desired directory for ebooks"
+        } else {
+            $EbookDirectoryCorrect = Confirm-UserChoice "Is this the correct base location to store the ebook in? $EbookDirectory (Y/N)"
+            if (-not $EbookDirectoryCorrect) {
+                $EbookDirectory = Read-Host "Enter the desired directory for ebooks"
+            }
         }
         $FolderPathEbook = Join-Path -Path $EbookDirectory -ChildPath "$Author\$BookTitle ($ReleaseYear)"
-        Save-Configuration
     }
+
+    # Save configuration after both directories have been verified
+    Save-Configuration
 
     # Check if both audiobook and ebook directories are being created in the same location
     if ($CreateAudiobook -and $CreateEbook -and $AudiobookDirectory -eq $EbookDirectory) {
@@ -98,6 +95,6 @@ do {
     }
 
     # Ask the user if they want to run the script again
-    $RunAgain = Validate-Input "Do you want to run this script again? (Y/N)"
+    $RunAgain = Confirm-UserChoice "Do you want to run this script again? (Y/N)"
 
 } while ($RunAgain)
